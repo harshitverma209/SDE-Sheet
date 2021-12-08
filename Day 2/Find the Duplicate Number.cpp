6c3e4f8935a31d4d920ef991a6ed346f4212e014
Harshit Verma <33396779+harshitverma209@users.noreply.github.com>
2021-12-08 16:32:41 +0530

    Added Find the Duplicate Number

---
+++ Day 2/Find the Duplicate Number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int fast=nums[0];
+        int slow=nums[0];
+        do{
+            fast=nums[nums[fast]];
+            slow=nums[slow];
+        }while(slow!=fast);
+        fast=nums[0];
+        while(fast!=slow){
+            fast=nums[fast];
+            slow=nums[slow];
+        }
+        return fast;
+    }
+};
